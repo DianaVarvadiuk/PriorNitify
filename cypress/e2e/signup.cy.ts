768579185b40b7ad6cf81f9799270a026d10f5e9
Diana Varvadiuk <dianavarvadiuk@gmail.com>
2022-07-18 17:34:27 +0300

    change name class on signpup

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -1,56 +1,56 @@
-import {SignInPage} from './page-object/SignUpPage'
+import {SignUpPage} from './page-object/SignUpPage'
 import HomePage from './page-object/HomePage'
 describe('Sign in page and check inputs', () => {
   it('Should sign in via english language', () => {
     const home = new HomePage();
     home.visit();
     //visit sign in page
-    const signin = new SignInPage()
-    signin.visit()
+    const signup = new SignUpPage()
+    signup.visit()
     cy.wait(2000)
     //write invalid email
-    signin.
+    signup.
         getEmail('dianagmail.com')
     cy.contains('This field must be a valid email address')
     //write invalid password
-    signin.
+    signup.
       getPassword('12345')
     cy.contains('Password must have at least 8 characters, including at least one upper case letter, at least one lower case letter, and at least one number.')
     //write invalid confirm password
-    signin
+    signup
       .getConfirmPassword('123')
     cy.contains('Password does not match')
-    signin
+    signup
       .getCheckbox()
       .click()
     //check if button is disabled
-    signin 
+    signup 
       .getCreateAccountBtn()
       .should('be.disabled')
     //write invalid email
-    signin
+    signup
       .getEmail('@gmail.com')
     cy.contains('This field must be a valid email address')
     //write empty password
-    signin
+    signup
       .getPassword('U12345678')
     cy.contains('Password must have at least 8 characters, including at least one upper case letter, at least one lower case letter, and at least one number.')
-    signin
+    signup
       .getConfirmPassword('231313')
     cy.contains('Password does not match')
-    signin.getCheckbox()
+    signup.getCheckbox()
     //check if button is disabled
-    signin
+    signup
       .getCreateAccountBtn()
       .should('be.disabled')
      //write valid email
-     signin
+     signup
      .getEmail('dianavarvadiuk@gmail.com')
      .getPassword('Tt12345678')
      .getConfirmPassword('Tt12345678')
      .getCheckbox()
 
-     signin
+     signup
       .getCreateAccountBtn()
       .click()
       .should('be.enabled')
@@ -59,9 +59,9 @@ describe('Sign in page and check inputs', () => {
     const home = new HomePage();
     home.visit();
     //visit sign in page
-    const signin = new SignInPage()
-    signin.visit()
-    signin
+    const signup = new SignUpPage()
+    signup.visit()
+    signup
       .getLanguage()
       .click()
     cy.getIframeBody('.goog-te-menu-frame.skiptranslate')
@@ -71,48 +71,48 @@ describe('Sign in page and check inputs', () => {
       .click()
     cy.wait(2000)
     //write invalid email
-    signin
+    signup
      .getEmail('користувачgmail.com')
     cy.contains('Це поле має бути дійсною електронною адресою')
     //write invalid password
-    signin
+    signup
       .getPassword('53453')
     cy.contains('Пароль має містити принаймні 8 символів, включаючи принаймні одну літеру верхнього регістру, принаймні одну літеру нижнього регістру та принаймні одну цифру.')
     //write invalid confirm password
-    signin
+    signup
       .getConfirmPassword('34453')
     cy.contains('Пароль не збігається')
-    signin 
+    signup 
       .getCheckbox()
       .click()
     //check if button is disabled
-    signin
+    signup
       .getCreateAccountBtn()
       .should('be.disabled')
     //write invalid email
-    signin
+    signup
       .getEmail('користувач')
     cy.contains('Це поле має бути дійсною електронною адресою')
     //write password
-    signin
+    signup
       .getPassword('Фф123456789')
     cy.contains('Пароль має містити принаймні 8 символів, включаючи принаймні одну літеру верхнього регістру, принаймні одну літеру нижнього регістру та принаймні одну цифру.')
-    signin
+    signup
       .getConfirmPassword('Фф12345678')
     cy.contains('Пароль не збігається')
-    signin
+    signup
       .getCheckbox()
     //check if button is disabled
-    signin
+    signup
       .getCreateAccountBtn()
       .should('be.disabled')
      //write valid email
-     signin
+     signup
       .getEmail('користувач@gmail.com')
       .getPassword('Ff123456789')
       .getConfirmPassword('Ff123456789')
       .getCheckbox()
-     signin
+      signup
       .getCreateAccountBtn().click()
        .should('be.enabled')
 
